49ab6dcbd09d9a1ea7151ab9bdf5d57633c30146
Samarth Bansal <samarthbansal_23me237@dtu.ac.in>
2024-05-26 17:45:16 +0530

    added the first page and cursor

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// gsap.from(".line h1",{
+function loadingAnimation (){
+    // gsap.from(".line h1",{
 //     y:150,
 //     stagger:0.27,
 //     duration:0.6,
@@ -40,14 +41,39 @@ tl.from(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:4
+    delay:2.5
 })
 tl.from("#page1",{
     delay:0.2,
     y:1600,
-    opacity:0,
+    duration:0.6,
     ease:Power4
 })
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+
+loadingAnimation()
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
+       
+      });
+}
+
+cursorAnimation()
